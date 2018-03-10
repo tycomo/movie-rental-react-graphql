45f1aab27a9ef6060237911702d47890b9e586ab
tylerjcomo@gmail.com <tylerjcomo@gmail.com>
2018-03-09 22:06:09 -0500

    Added moviedb api

--- movie-rental-react-graphql/rental/schema.py
+++ movie-rental-react-graphql/rental/schema.py
@@ -2,13 +2,37 @@ import graphene
 from graphene_django.types import DjangoObjectType
 from graphql_relay.node.node import from_global_id
 import json
+import requests
+from collections import namedtuple
 
 from . import models
+
+
+def _json_object_hook(d):
+    return namedtuple('X', d.keys())(*d.values())
+
+def json2obj(data):
+    return json.loads(data, object_hook=_json_object_hook)
 
 class MovieRentalType(DjangoObjectType):
     class Meta:
         model = models.MovieRental
         interfaces = (graphene.Node, )
+
+class PopularMovieType(graphene.ObjectType):
+    vote_count = graphene.Int()
+    id = graphene.ID()
+    video = graphene.Boolean()
+    vote_average = graphene.Float()
+    title = graphene.String()
+    popularity = graphene.Float()
+    poster_path = graphene.String()
+    original_language = graphene.String()
+    original_title = graphene.String()
+    backdrop_path = graphene.String()
+    adult = graphene.Boolean()
+    overview = graphene.String()
+    release_date = graphene.String()
 
 class Query(graphene.ObjectType):
     all_rentals = graphene.List(MovieRentalType)
@@ -21,6 +45,16 @@ class Query(graphene.ObjectType):
     def resolve_rental(self, info, **args):
         rid = from_global_id(args.get('id'))
         return models.MovieRental.objects.get(pk=rid[1])
+
+    popular_movies = graphene.List(PopularMovieType)
+
+    def resolve_popular_movies(self, info, **args):
+        print("this is called")
+        popular = requests.get('https://api.themoviedb.org/3/movie/popular?api_key=8879bf0d7d0370ed12d9245c5c774ae1&language=en-US&page=1')
+        content = json.loads(popular.content)["results"]
+        jsonout = json2obj(json.dumps(content))
+        return jsonout
+
 
 class CreateRentalMutation(graphene.Mutation):
     class Arguments:
