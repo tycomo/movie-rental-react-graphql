5f9b7e45e73b186fb8c34afaf964c430f284cbf5
Tyler Como <tyler.como@matric.com>
2018-03-28 17:00:37 -0400

    Updated Settings

--- movie-rental-react-graphql/settings.py
+++ movie-rental-react-graphql/settings.py
@@ -65,9 +65,10 @@ JWT_VERIFY_EXPIRATION = False
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://movie-rental.qsewkd.0001.use2.cache.amazonaws.com:6379/0',
+        'LOCATION': 'redis://tjc-movies.redis.cache.windows.net:6379/',
         'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'PASSWORD': 'TyCHVHWJoNW8LjfJHqCbnHC6gIpvd9zDgRMjjfexXz4='
         }
     }
 }
